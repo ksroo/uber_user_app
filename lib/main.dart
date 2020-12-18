fa2cb05d45549f4605fa34211044492fc3efae7c
walid asroo <elm3lamksroo@gmail.com>
2020-12-18 18:44:34 +0200

    firebase font images

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-   
+        fontFamily: "Signatra",
         primarySwatch: Colors.blue,
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -21,4 +22,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-}
